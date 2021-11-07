c760551e319b6bcb4ba5453a0aa6952f1c03a403
William Erthal <83590841+williamerthal@users.noreply.github.com>
2021-11-07 06:58:55 -0300

    alteracao 211106 06-58

--- lampada.js
+++ lampada.js
@@ -27,3 +27,29 @@ turnOff.addEventListener("click", lampOff);
 lamp.addEventListener("mouseover", lampOn);
 lamp.addEventListener("mouseleave", lampOff);
 lamp.addEventListener("dblclick", lampBroken);
+
+function isLampBroken2() {
+  return lamp2.src.indexOf("quebrada") > -1;
+}
+
+function lampOn2() {
+  if (!isLampBroken2()) {
+    lamp2.src = "./img/ligada.jpg";
+  }
+}
+
+function lampOff2() {
+  if (!isLampBroken2()) {
+    lamp2.src = "./img/desligada.jpg";
+  }
+}
+
+function lampBroken2() {
+  lamp2.src = "./img/quebrada.jpg";
+}
+
+turnOn2.addEventListener("click", lampOn2);
+turnOff2.addEventListener("click", lampOff2);
+lamp2.addEventListener("mouseover", lampOn2);
+lamp2.addEventListener("mouseleave", lampOff2);
+lamp2.addEventListener("dblclick", lampBroken2);
