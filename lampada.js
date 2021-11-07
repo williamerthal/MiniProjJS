186925bfb459d218c68fb92e81e9debefd66e54b
William Erthal <83590841+williamerthal@users.noreply.github.com>
2021-11-07 04:30:37 -0300

    altera-01 acertos em erros de escrita no arquivo

--- lampada.js
+++ lampada.js
@@ -1,32 +1,30 @@
 const turnOn = document.getElementById("turnOn");
 const turnOff = document.getElementById("turnOff");
-const lamp = docuemnt.getElementById("lamp");
- 
+const lamp = document.getElementById("lamp");
+
 function isLampBroken() {
- return lamp.src.indexOf("quebrada") > -1;
+  return lamp.src.indexOf("quebrada") > -1;
 }
- 
+
 function lampOn() {
- if (!isLampBroken()) {
-   lamp.src = "./img/lamp_ligada.jpg";
- }
+  if (!isLampBroken()) {
+    lamp.src = "./img/lamp_ligada.jpg";
+  }
 }
- 
+
 function lampOff() {
- if (!isLampBroken()) {
-   lamp.src = "./img/lamp_desligada.jpg";
- }
+  if (!isLampBroken()) {
+    lamp.src = "./img/lamp_desligada.jpg";
+  }
 }
- 
+
 function lampBroken() {
- if (!isLampBroken()) {
-   lamp.src = "./img/lamp_quebrada.jpg";
- }
+  lamp.src = "./img/lamp_quebrada.jpg";
 }
- 
+
 turnOn.addEventListener("click", lampOn);
 turnOff.addEventListener("click", lampOff);
- 
+
 lamp.addEventListener("mouse over", lampOn);
 lamp.addEventListener("mouse leave", lampOff);
-lamp.addEventListener("dbclick", lampBrpken);
+lamp.addEventListener("dbclick", lampBroken);
